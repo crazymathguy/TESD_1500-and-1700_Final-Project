8c990e4efde9769cc47c8aaa1b4447b86c7ed6c9
Sean Briggs <briggs.sea0642@stu.stech.edu>
2026-02-19 11:15:42 -0700

    Worked on Login page

---
+++ lib/library_management.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 19, 2026 at 06:11 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `library_management`
+--
+CREATE DATABASE IF NOT EXISTS `library_management` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `library_management`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_info`
+--
+
+CREATE TABLE `login_info` (
+  `username` char(20) NOT NULL,
+  `password` char(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login_info`
+--
+ALTER TABLE `login_info`
+  ADD PRIMARY KEY (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- Privileges for `webuser`@`localhost`
+
+GRANT USAGE ON *.* TO 'webuser'@'localhost' IDENTIFIED BY 'myL!brary73';
+
+GRANT SELECT, INSERT, REFERENCES ON `library_management`.`login_info` TO 'webuser'@'localhost';
--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,25 +20,90 @@
 		<h1>Sean's Library Management</h1>
 	</header>
 	<?php
-		if (!isset($_GET['new'])) {
-			$title = 'Please Log In';
-			$link = 'Don\'t have an account? <a href="login.php?new=true">Sign Up</a>';
+		if (!isset($_POST['user']) && !isset($_POST['pass'])) {
+			if (isset($_GET['new'])) {
+				$heading = 'Create an Account';
+				$switchPage = 'Already have an account? <a href="login.php">Log In</a>';
+			}
+			else {
+				$heading = 'Please Log In';
+				$switchPage = 'Don\'t have an account? <a href="login.php?new=true">Create an Account</a>';
+			}
+			echo '<h2>'.$heading.'</h2>';
+			?>
+			<form action="login.php" method="post">
+				<label for="user">Username:</label>
+				<input type="text" name="user" id="user" size="20" maxlength="30" required /><br />
+				<label for="pass">Password:</label>
+				<input type="password" name="pass" id="pass" size="20" required /><br />
+				<?php
+					echo '<input type="hidden" name="new" value="'.isset($_GET['new']).'" />';
+				?>
+				<input type="submit" value="Login" />
+			</form>
+			<?php
+			echo '<p>'.$switchPage.'</p>';
 		}
 		else {
-			$title = 'Create an Account';
-			$link = 'Already have an account? <a href="login.php">Log In</a>';
+			try {
+				require('lib/database_access.inc');
+
+				echo '<p>Connected</p>';
+				
+				$user = trim($_POST['user']);
+				$pass = password_hash(trim($_POST['pass']), PASSWORD_DEFAULT);
+			}
+			catch (Exception $e) {
+				echo '<p>An error occurred.<br />Message: '.$e->message.'.</p>';
+			}
+			if ($_POST['new']) {
+				$query = "SELECT username FROM login_info
+				WHERE username = ?";
+				$stmt = $db->prepare($query);
+				$stmt->bind_param('s', $user);
+				$stmt->execute();
+				$stmt->store_result();
+
+				if ($stmt->num_rows > 0) {
+					echo '<p><strong>That username is already taken.</strong><br />
+						Please return to the previous page and try again.</p>';
+					exit;
+				}
+
+				$query = "INSERT INTO login_info VALUES (?, ?)";
+				$stmt = $db->prepare($query);
+				$stmt->bind_param('ss', $user, $pass);
+				$stmt->execute();
+
+				if ($stmt->affected_rows == 0) {
+					echo '<p>An error occurred trying to create your account.<br />
+						Please try again later.</p>';
+					exit;
+				}
+
+				$_SESSION['username'] = $user;
+				$db->close();
+
+				echo '<p>Yay! New User!</p>';
+			}
+			else {
+				$query = "SELECT `password` FROM login_info
+				WHERE username = ?";
+				$stmt = $db->prepare($query);
+				$stmt->bind_param('s', $user);
+				$stmt->execute();
+				$stmt->store_result();
+				$stmt->bind_result($hash);
+
+				if ($stmt->num_rows == 0 || !password_verify($pass, $hash)) {
+					echo '<p>Your username or password is incorrect.<br />
+						Please return to the previous page and try again.</p>';
+					exit;
+				}
+
+				echo '<p>Yay! You\'re back!</p>';
+			}
 		}
-		echo '<h2>'.$title.'</h2>';
-	?>
-	<form action="login.php" method="post">
-		<label for="user">Username:</label>
-		<input type="text" name="user" id="user" size="20" maxlength="30" /><br />
-		<label for="pass">Password:</label>
-		<input type="password" name="pass" id="pass" size="20" /><br />
-		<input type="submit" value="Login" />
-	</form>
-	<?php
-		echo '<p>'.$link.'</p>';
 	?>
 </body>
 </html>
