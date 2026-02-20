dbbdc7c5f149e84b9165e11e960097b067b14602
Sean Briggs <iluv2program@gmail.com>
2026-02-20 14:22:50 -0700

    enlarged username limit

--- lib/library_management.sql
+++ lib/library_management.sql
@@ -30,7 +30,7 @@ USE `library_management`;
 --
 
 CREATE TABLE `login_info` (
-  `username` char(20) NOT NULL,
+  `username` char(30) NOT NULL,
   `password` char(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
