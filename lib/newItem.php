a282745c27211434881ebc3afffa0a8309aabec9
Sean Briggs <iluv2program@gmail.com>
2026-03-05 10:12:38 -0700

    Created new item submission

---
+++ lib/newItem.php
@@ -0,0 +1,25 @@
+<!--
+	Library Management New Item Page
+	Author: Sean Briggs
+	Date Created: 2026-02-17
+
+	Filename: newItem.php
+-->
+
+<?php
+	session_start();
+	/* if (!isset($_SESSION['user'])) {
+		header("Location: login.php");
+	} */
+	require('database_access.inc');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Personal Library Manager</title>
+</head>
+</html>
+
+<script>
+	window.close();
+</script>
--- library.php
+++ library.php
@@ -16,15 +16,27 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<script src="lib/js/newItem.js" defer></script>
 	<title>Personal Library Manager</title>
 </head>
 
 <body>
 	<header>
 		<h1>Personal Library Management System</h1>
-	</header>
+		<?php
+			echo '<h2>Welcome '.$_SESSION['user'].'!</h2>';
+			echo '</header>';
+
+			$db = new mysqli($hostname, $username, $password, $dbname);
+			if (mysqli_connect_errno()) {
+				echo '<p>Couldn\'t connect to the database.<br />
+					Please try again later</p>';
+				exit;
+			}
+		?>
 	<h2>Your Library</h2>
-	<table>
+	<input type="button" name="new" id="new" value="Add New Item" onclick="AddNew()" />
+	<table id="collectionTable">
 		<tr>
 			<th>ISBN</th>
 			<th>Author</th>
@@ -32,6 +44,27 @@
 			<th>Shelf Location</th>
 			<th>Item Location</th>
 		</tr>
+		<?php
+			$query = "SELECT ISBN, Author, Title, `Shelf Location`, `Item Location`
+				FROM `".$_SESSION['user']."` LIMIT 25";
+			$stmt = $db->prepare($query);
+			$stmt->execute();
+			$stmt->store_result();
+			$stmt->bind_result($isbn, $author, $title, $shelf, $item);
+
+			while ($stmt->fetch()) {
+				echo '<tr>';
+				echo '<td>'.$isbn.'</td>';
+				echo '<td>'.$author.'</td>';
+				echo '<td>'.$title.'</td>';
+				echo '<td>'.$shelf.'</td>';
+				echo '<td>'.$item.'</td>';
+				echo '</tr>';
+			}
+			$stmt->close();
+
+			$db->close();
+		?>
 	</table>
 </body>
 </html>
