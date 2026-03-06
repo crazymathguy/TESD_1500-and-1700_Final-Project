68da4b296f43edfba562a70aabf5f8a4f304c822
Sean Briggs <iluv2program@gmail.com>
2026-03-06 14:57:16 -0700

    New Items Update Database

--- lib/newItem.php
+++ lib/newItem.php
@@ -12,13 +12,44 @@
 		header("Location: login.php");
 	} */
 	require('database_access.inc');
+
+	$db = new mysqli($hostname, $username, $password, $dbname);
+	if (mysqli_connect_errno()) {
+		echo '<p>Couldn\'t connect to the database.<br />
+			Please try again later</p>';
+		exit;
+	}
+
+	echo 'start';
+	$isbn = htmlspecialchars(trim($_POST['isbn']));
+	$author = htmlspecialchars(trim($_POST['author']));
+	$title = htmlspecialchars(trim($_POST['title']));
+	$shelf = htmlspecialchars(trim($_POST['shelf']));
+	$item = intval($_POST['item']);
+	echo $isbn;
+	echo $author;
+	echo $title;
+	echo $shelf;
+	echo $item;
+
+	$query = "INSERT INTO `".$_SESSION['user']."`
+		(ISBN, Author, Title, `Shelf Location`, `Item Location`)
+		VALUES (?, ?, ?, ?, ?)";
+	echo $query;
+	$stmt = $db->prepare($query);
+	echo '<br />create query';
+	$stmt->bind_param('ssssi', $isbn, $author, $title, $shelf, $item);
+	if (!$stmt->execute()) {
+		$errno = $stmt->errno;
+		$err = $stmt->error;
+		echo '<p>Database error ('.htmlspecialchars($errno).'): <br />'
+			.htmlspecialchars($err).'</p>';
+		$stmt->close();
+		exit;
+	}
+	$stmt->close();
+	$db->close();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Personal Library Manager</title>
-</head>
-</html>
 
 <script>
 	window.close();
